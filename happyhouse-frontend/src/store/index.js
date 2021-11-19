b2ac9e2d8e0536fb24165d36c798c29aa1993b96
yunhwan <rhddbsghks@ajou.ac.kr>
2021-11-19 16:27:36 +0900

    Main.vue 수정

--- happyhouse-frontend/src/store/index.js
+++ happyhouse-frontend/src/store/index.js
@@ -4,27 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 import createPersistedState from "vuex-persistedstate";
-<<<<<<< HEAD
 import memberStore from "@/store/modules/memberStore.js";
-
-export default new Vuex.Store({
-  modules: {
-    memberStore,
-=======
 import mapStore from "@/store/modules/mapStore.js";
 
 const store = new Vuex.Store({
   modules: {
+    memberStore,
     mapStore,
->>>>>>> c5866bef93cf64cf299d949e416f97c2e8fe0ae1
   },
   plugins: [
     createPersistedState({
       // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
-<<<<<<< HEAD
-      // storage: sessionStorage,
-=======
->>>>>>> c5866bef93cf64cf299d949e416f97c2e8fe0ae1
       storage: sessionStorage,
     }),
   ],
--- happyhouse-frontend/src/views/Main.vue
+++ happyhouse-frontend/src/views/Main.vue
@@ -18,13 +18,10 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import { mapState, mapActions } from "vuex";
 const memberStore = "memberStore";
-
-=======
 import MapMain from "@/components/main/MapMain.vue";
->>>>>>> c5866bef93cf64cf299d949e416f97c2e8fe0ae1
+
 export default {
   computed: {
     ...mapState(memberStore, ["userInfo"]),
@@ -61,17 +58,16 @@ export default {
       ],
     };
   },
-<<<<<<< HEAD
+
   methods: {
     ...mapActions(memberStore, ["logout"]),
     logoutProcess() {
       this.logout();
       this.$router.push({ name: "Home" });
     },
-=======
+  },
   components: {
     MapMain,
->>>>>>> c5866bef93cf64cf299d949e416f97c2e8fe0ae1
   },
 };
 </script>
