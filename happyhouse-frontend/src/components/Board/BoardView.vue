ecd708e4ef64d34c838861c03575bf05a5cd0a17
yunhwan <rhddbsghks@ajou.ac.kr>
2021-11-23 14:38:13 +0900

    게시판 댓글 추가

--- happyhouse-frontend/src/components/Board/BoardView.vue
+++ happyhouse-frontend/src/components/Board/BoardView.vue
@@ -38,7 +38,7 @@
           >
           <span style="width: 150px"
             >댓글: <i class="fa fa-commenting-o" aria-hidden="true"></i>
-            {{ board.comment.length }}건</span
+            {{ comments.length }}건</span
           >
         </section>
 
@@ -60,17 +60,103 @@
         </section>
 
         <!-- 댓글 리스트 -->
+        <span style="float: left">댓글작성</span>
+        <section
+          id="bo_vc"
+          style="margin-bottom: 40px; width: 100%"
+          class="d-flex justify-content-between"
+        >
+          <b-form-group
+            id="fieldset-horizontal"
+            style="text-align: left; width: 80%; margin: 0px"
+          >
+            <b-form-textarea
+              id="textarea-rows"
+              rows="3"
+              v-model="newComment"
+            ></b-form-textarea>
+          </b-form-group>
+          <b-button
+            class="btn-lg"
+            variant="outline-info"
+            style="width: 15%"
+            @click="registComment"
+            >작성</b-button
+          >
+        </section>
+
+        <!-- 댓글 리스트 -->
         <section id="bo_vc" style="margin-bottom: 80px">
-          <h4 style="text-align: left">댓글목록</h4>
+          <h4 style="text-align: left">
+            <i class="far fa-comment"></i> 댓글목록
+          </h4>
           <hr />
-          <p id="bo_vc_empty" style="color: #d15ca4">등록된 댓글이 없습니다.</p>
-          <div>댓글을</div>
-          <div>댓글을</div>
-          <div>댓글을</div>
-          <div>댓글을</div>
-          <div>댓글을</div>
-          <div>댓글을</div>
-          <div>댓글을</div>
+          <p
+            v-if="comments.length == 0 || comments == null"
+            style="color: #d15ca4"
+          >
+            등록된 댓글이 없습니다.
+          </p>
+          <div
+            style="text-align: left; margin: 20px 0"
+            v-for="(comment, index) in this.comments"
+            :key="comment.id"
+          >
+            <div style="font-size: 0.8em">
+              {{ comment.name }}({{ comment.id }})
+              <span style="margin-left: 5px; font-size: 0.9em; color: gray">{{
+                comment.regTime
+              }}</span>
+
+              <span v-if="comment.id == userInfo.id">
+                <span
+                  v-if="!comment.isModifing"
+                  style="font-size: 0.7em; padding: 0 3px; padding-left: 10px"
+                  class="btn"
+                  @click="toggleModifyComment(index)"
+                  >수정하기</span
+                >
+                <span
+                  v-else
+                  style="font-size: 0.7em; padding: 0 3px; padding-left: 10px"
+                  class="btn"
+                  @click="toggleModifyComment(index)"
+                  >취소</span
+                >
+                <span
+                  style="font-size: 0.7em; padding: 0 3px"
+                  class="btn"
+                  @click="deleteComment(index)"
+                  >삭제하기</span
+                >
+              </span>
+            </div>
+            <div
+              v-if="comment.isModifing"
+              id="bo_vc"
+              class="d-flex justify-content-between"
+            >
+              <b-form-group
+                id="fieldset-horizontal"
+                style="text-align: left; width: 80%; margin: 0px"
+              >
+                <b-form-textarea
+                  id="textarea-rows"
+                  rows="1"
+                  v-model="comment.content"
+                ></b-form-textarea>
+              </b-form-group>
+              <b-button
+                class="btn-lg"
+                variant="outline-info"
+                style="width: 15%"
+                @click="updateComment(index)"
+                >수정</b-button
+              >
+            </div>
+
+            <div v-else style="font-size: 1.1em">{{ comment.content }}</div>
+          </div>
         </section>
       </article>
     </div>
@@ -83,7 +169,7 @@ import { mapState } from "vuex";
 const memberStore = "memberStore";
 
 export default {
-  name: "QnAView",
+  name: "BoardView",
   data() {
     return {
       board: {
@@ -93,8 +179,9 @@ export default {
         regtime: "",
         content: "",
         view: "",
-        comment: [{ id: "hel", name: "김김", com: "ㅋㅋㅋ" }],
       },
+      comments: [],
+      newComment: "",
     };
   },
   computed: {
@@ -108,8 +195,9 @@ export default {
       this.board.regtime = data.regtime;
       this.board.content = data.content;
       this.board.view = data.view;
-      console.log(data);
     });
+
+    this.loadComment();
   },
   methods: {
     deleteBoard() {
@@ -130,6 +218,54 @@ export default {
         name: "BoardModify",
         params: { no: this.$route.params.no },
       });
+    },
+    loadComment() {
+      this.comments = [];
+      http.get(`/api/comment/${this.$route.params.no}`).then(({ data }) => {
+        data.forEach((d) => {
+          this.comments.push({
+            commentNo: d.commentNo,
+            noticeNo: d.noticeNo,
+            id: d.id,
+            content: d.content,
+            name: d.name,
+            regTime: d.regTime,
+            isModifing: false,
+          });
+        });
+      });
+    },
+
+    registComment() {
+      http
+        .post(`/api/comment`, {
+          noticeNo: this.$route.params.no,
+          id: this.userInfo.id,
+          content: this.newComment,
+        })
+        .then(() => {
+          this.newComment = "";
+          this.loadComment();
+        });
+    },
+    updateComment(index) {
+      http
+        .put(`/api/comment`, {
+          commentNo: this.comments[index].commentNo,
+          content: this.comments[index].content,
+        })
+        .then(() => {
+          this.loadComment();
+        });
+    },
+
+    deleteComment(index) {
+      http.delete(`/api/comment/${this.comments[index].commentNo}`).then(() => {
+        this.loadComment();
+      });
+    },
+    toggleModifyComment(index) {
+      this.comments[index].isModifing = !this.comments[index].isModifing;
     },
   },
 };
