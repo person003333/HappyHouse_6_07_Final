ee9df20de7172be6660bab35facf1cfd46161d8d
이정훈 <person00333@gmail.com>
2021-11-25 11:25:27 +0900

    로드뷰

--- happyhouse-frontend/src/components/MapMain/HouseDetail.vue
+++ happyhouse-frontend/src/components/MapMain/HouseDetail.vue
@@ -22,10 +22,15 @@
           />
         </b-col>
       </b-row>
-      <b-row class="d-flex x justify-content-center mb-4"
+      <!-- <b-row class="d-flex x justify-content-center mb-4"
         ><b-col cols="10" align-self="stretch"
           ><b-img :src="require('@/assets/apt.png')" fluid-grow></b-img></b-col
-      ></b-row>
+      ></b-row> -->
+      <b-row class="mb-4">
+        <b-col cols="12" align-self="stretch">
+          <div ref="roadview" style="width: 100%; height: 300px"></div>
+        </b-col>
+      </b-row>
       <b-row class="mb-2 mt-1">
         <b-col>
           <table class="table" style="border: 1px solid #dee2e6">
@@ -182,6 +187,9 @@ export default {
       check_interest: false,
     };
   },
+  mounted() {
+    this.show_road_view();
+  },
   created() {
     let sub = this.subway_text.split(` (`);
     this.subway = sub[0];
@@ -233,11 +241,47 @@ export default {
         }
       }
     },
+
+    show_road_view() {
+      var rvContainer = this.$refs.roadview; // 로드뷰를 표시할 div
+
+      var rv = new window.kakao.maps.Roadview(rvContainer); // 로드뷰 객체 생성
+      var rc = new window.kakao.maps.RoadviewClient(); // 좌표를 통한 로드뷰의 panoid를 추출하기 위한 로드뷰 help객체 생성
+      var rvPosition = new window.kakao.maps.LatLng(
+        this.house.lat,
+        this.house.lng
+      );
+
+      rc.getNearestPanoId(rvPosition, 50, function (panoid) {
+        rv.setPanoId(panoid, rvPosition); //좌표에 근접한 panoId를 통해 로드뷰를 실행합니다.
+      });
+
+      // 로드뷰 초기화 이벤트
+      window.kakao.maps.event.addListener(rv, "init", function () {
+        // 로드뷰에 올릴 마커를 생성합니다.
+        var rMarker = new window.kakao.maps.Marker({
+          position: rvPosition,
+          map: rv, //map 대신 rv(로드뷰 객체)로 설정하면 로드뷰에 올라갑니다.
+        });
+        rMarker.setAltitude(6); //마커의 높이를 설정합니다. (단위는 m입니다.)
+        rMarker.setRange(100); //마커가 보일 수 있는 범위를 설정합니다. (단위는 m입니다.)
+        // 로드뷰 마커가 중앙에 오도록 로드뷰의 viewpoint 조정합니다.
+        var projection = rv.getProjection(); // viewpoint(화면좌표)값을 추출할 수 있는 projection 객체를 가져옵니다.
+
+        // 마커의 position과 altitude값을 통해 viewpoint값(화면좌표)를 추출합니다.
+        var viewpoint = projection.viewpointFromCoords(
+          rMarker.getPosition(),
+          rMarker.getAltitude()
+        );
+        rv.setViewpoint(viewpoint); //로드뷰에 뷰포인트를 설정합니다.
+      });
+    },
   },
   watch: {
     house(newVal) {
       console.log(newVal);
       this.check_myInterest();
+      this.show_road_view();
     },
     interestedApt() {
       this.check_myInterest();
